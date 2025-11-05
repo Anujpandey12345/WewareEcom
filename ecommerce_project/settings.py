1dc4e30a1ab9075645b022ec0867805f531deb30
Anuj Kumar <anujpandey61687@gmail.com>
2025-11-05 14:39:32 +0530

    Fix static files for Render

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import dj_database_url
 from decouple import config
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'ecommerce_project.urls'
@@ -118,9 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-# Folder where Django collects all static files when you run collectstatic
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Required for collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 # Folders where Django will search for static files during development
 STATICFILES_DIRS = [
